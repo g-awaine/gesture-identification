0254283e759b9e2bbbf675d65a8f10a4b727a989
khan <sg.limkhan@gmail.com>
2026-06-27 21:55:00 +0800

    Added hand signs function

--- gesture-identification.py
+++ gesture-identification.py
@@ -1,12 +1,58 @@
-from modules import Webcam
+import modules as m
+import mediapipe as mp
 import cv2 as cv
 
 # Initialise the webcam
-webcam = Webcam(cam=0)
+webcam = m.Webcam(cam=0)
 
+#detects hand object and draws
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+hands = mp_hands.Hands()
+
+calling = [0, None]
+frames_to_call = 5
 while True:
     # Get the frame
     frame = webcam.get_frame()
+
+    results = hands.process(frame)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            thumb = m.finger_up(hand_landmarks, 4, 3)
+            index = m.finger_up(hand_landmarks, 8, 6)
+            middle = m.finger_up(hand_landmarks, 12, 10)
+            ring = m.finger_up(hand_landmarks, 16, 14)
+            pinky = m.finger_up(hand_landmarks, 20, 18)
+            thumb = m.finger_up(hand_landmarks, 4, 1)
+
+            gesture = None
+
+            if index and not middle and not ring and not pinky:
+                gesture = "index_up"
+
+            if not index and middle and ring and pinky:
+                gesture = 'index_down'
+
+            if middle and not index and not ring and not pinky:
+                webcam.release()
+                break
+
+            if calling[1] == gesture:
+                calling[0] += 1
+            else:
+                calling = [1, gesture]
+
+            if calling[0] == frames_to_call:
+                if gesture in m.gesture_actions:
+                    m.gesture_actions[gesture]()
+                    calling = [0, None]
+
+            mp_draw.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS
+            )
 
     # Shows frame
     webcam.show_frame(frame)
@@ -15,4 +61,3 @@ while True:
     if cv.waitKey(1) == ord('q'):
         webcam.release()
         break
-
--- modules.py
+++ modules.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+from pycaw.pycaw import AudioUtilities
 
 class Webcam():
     def __init__(self, cam:int=0):
@@ -21,9 +22,33 @@ class Webcam():
     def show_frame(self, frame):
         # Display the frame
         cv.imshow('Gesture Detection', frame)
-
     
     def release(self): 
         # When everything done, release the capture
         self.cap.release()
         cv.destroyAllWindows()
+
+#Volume control
+device = AudioUtilities.GetSpeakers()
+volume = device.EndpointVolume
+
+def volume_up(step=0.05):
+    current = volume.GetMasterVolumeLevelScalar()
+    volume.SetMasterVolumeLevelScalar(min(current + step, 1.0), None)
+
+def volume_down(step=0.05):
+    current = volume.GetMasterVolumeLevelScalar()
+    volume.SetMasterVolumeLevelScalar(max(current - step, 0.0), None)
+#-----------------------------------------------------
+
+#Functions for each hand sign
+def finger_up(hand, tip, pip):
+    return hand.landmark[tip].y < hand.landmark[pip].y
+#-----------------------------------------------------
+
+#Dictionary representing what to do
+gesture_actions = {
+    "index_down": volume_down,
+    "index_up": volume_up,
+}
+#------------------------------------------------------
