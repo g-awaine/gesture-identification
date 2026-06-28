610f21fda56555c39c6a268e1a32e156c586eaeb
khan <sg.limkhan@gmail.com>
2026-06-28 16:03:24 +0800

    Added motion detection for left hand

--- gesture-identification.py
+++ gesture-identification.py
@@ -12,13 +12,19 @@ hands = mp_hands.Hands()
 
 calling = [0, None]
 frames_to_call = 5
+game = False
+frames = 0
+prev_x = None
+prev_y = None
+
+
 while True:
     # Get the frame
     frame = webcam.get_frame()
 
     results = hands.process(frame)
     if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
+        for hand_landmarks, handedness in zip(results.multi_hand_landmarks,results.multi_handedness):
             thumb = m.finger_up(hand_landmarks, 4, 3)
             index = m.finger_up(hand_landmarks, 8, 6)
             middle = m.finger_up(hand_landmarks, 12, 10)
@@ -27,16 +33,45 @@ while True:
             thumb = m.finger_up(hand_landmarks, 4, 1)
 
             gesture = None
+            hand = handedness.classification[0].label
 
-            if index and not middle and not ring and not pinky:
-                gesture = "index_up"
+            #Gestures for right hand just for increasing/decreasing volume
+            if hand == "Left":
+                if index and not middle and not ring and not pinky:
+                    gesture = "index_up"
 
-            if not index and middle and ring and pinky:
-                gesture = 'index_down'
+                if not index and middle and ring and pinky:
+                    gesture = 'index_down'
 
-            if middle and not index and not ring and not pinky:
-                webcam.release()
-                break
+                if pinky and not index and not ring and not middle:
+                    webcam.release()
+                    break
+
+            #Gestures for left hand, for motion detection, just to see how still you can keep your hand
+            if hand == "Right":
+                if index and middle and ring and pinky and not game:
+                    game = True
+                    frames = 0
+                    prev_x = None
+                    prev_y = None
+
+                if game:
+                    wristx, wristy = hand_landmarks.landmark[0].x, hand_landmarks.landmark[0].y
+                    frames += 1
+
+                if prev_x is not None:
+                    dx = wristx - prev_x
+                    dy = wristy - prev_y
+
+                    if abs(dx) > 0.05 or abs(dy) > 0.05:
+                        print("Moved!")
+                        print("Held still for:", frames)
+
+                        game = False
+                        frames = 0
+
+                prev_x = wristx
+                prev_y = wristy
 
             if calling[1] == gesture:
                 calling[0] += 1
